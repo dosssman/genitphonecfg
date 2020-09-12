0cb7f3ca8b2e163c530a862c8f58372f791fe14b
dosssman <dosssman@hotmail.fr>
2020-09-12 11:55:35 +0900

    Additional fixes, fwd session killer

--- fwdshvnc.sh
+++ fwdshvnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pkill -f "ssh -L 25324:localhost:25324 foster -fNT"
-pkill -f "ssh -L 25850:localhost:25850 foster -fNT"
+pkill -f "ssh -L 25324:127.0.0.1:25324 foster -fNT"
+pkill -f "ssh -L 25850:127.0.0.1:25850 foster -fNT"
 
-ssh -L 25324:localhost:25324 foster -fNT # then run ssh localhost -p 33222 on your side to enter the rev shelll
-ssh -L 25850:localhost:25850 foster -fNT # then run ssh localhost -p 33222 on your side to enter the rev shelll
+ssh -L 25324:127.0.0.1:25324 foster -fNT # then run ssh 127.0.0.1 -p 33222 on your side to enter the rev shelll
+ssh -L 25850:127.0.0.1:25850 foster -fNT # then run ssh 127.0.0.1 -p 33222 on your side to enter the rev shelll
