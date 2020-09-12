ff120cad5e3bbbf279ee3d3d6bc4cbab12d7da9b
dosssman <dosssman@hotmail.fr>
2020-09-12 12:20:05 +0900

    killfwd fix up

--- killfwdshvnc.sh
+++ killfwdshvnc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pkill -f "ssh -L 25324:localhost:25324 foster -fNT"
-pkill -f "ssh -L 25850:localhost:25850 foster -fNT"
+pkill -f "ssh -L 25324:127.0.0.1:25324 foster -fNT"
+pkill -f "ssh -L 25850:127.0.0.1:25850 foster -fNT"
