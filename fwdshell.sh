da50cc7007366db1ef5d4ce1136ad4be6d963790
dosssman <dosssman@hotmail.fr>
2020-09-12 10:23:56 +0900

    Edited the proper ports for the forward seesion

--- fwdshell.sh
+++ fwdshell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -L 33222:localhost:33222 foster # then run ssh localhost -p 33222 on your side to enter the rev shelll
+ssh -L 25324:localhost:25324 foster # then run ssh localhost -p 33222 on your side to enter the rev shelll
--- fwdvnc.sh
+++ fwdvnc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -L 5901:localhost:5901 foster # then run ssh localhost -p 33222 on your side to enter the rev shelll
+ssh -L 57000:localhost:5901 foster # then run ssh localhost -p 33222 on your side to enter the rev shelll
