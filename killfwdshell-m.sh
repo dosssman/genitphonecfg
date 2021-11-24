10a9915acc9c7b2076164dbd10326b30f3cd7993
dosssman <dosssman@hotmail.fr>
2021-11-24 15:04:42 +0100

    fleshed out the killfwdshell-*.sh variants

--- killfwdshell-m.sh
+++ killfwdshell-m.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pkill -f "ssh -L 25324:127.0.0.1:25324 foster -fNT"
-pkill -f "ssh -L 25850:127.0.0.1:25850 foster -fNT"
+REVERSE_SHELL_PORT=$(cat ./m/DEFAULT_PORT)
+REMOTE_HOSTNAME=$(cat ./REMOTE_HOSTNAME)
+
+pkill -f "ssh -L ${REVERSE_SHELL_PORT}:127.0.0.1:${REVERSE_SHELL_PORT} root@${REMOTE_HOSTNAME} -fNT"
